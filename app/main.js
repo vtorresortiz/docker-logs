c7c2da1d214560c1e6c4b0d9b4e96a2afbfce343
Victor Hugo Torres Ortiz <vito.ubb@gmail.com>
2021-03-22 11:03:38 -0300

    update plugin

--- app/main.js
+++ app/main.js
@@ -1,7 +1,9 @@
-let stop = false;
+const rand = Math.random();
 const main = async() => {
+    let stop = false;
+    let count = 1;
     while (true && !stop) {
-        console.log(`{"event": "hello world", "rand": ${Math.random()}}`);
+        console.log(`{"event": "hello world", "rand": ${rand}, "count": ${count++}}`);
         await new Promise(r => setTimeout(r, 500));
     }
 }
