e8e4742b56aea9abfb3cd51526fcbffb29809a2e
Victor Hugo Torres Ortiz <vito.ubb@gmail.com>
2021-03-25 11:03:25 -0300

    set KV_MODE

--- app/main.js
+++ app/main.js
@@ -3,8 +3,10 @@ const main = async() => {
     let stop = false;
     let count = 1;
     while (true && !stop) {
-        console.log(`{"event": "hello world", "rand": ${rand}, "count": ${count++}}`);
+        const log = {"event": "hello world", rand, count};
+        console.log(log);
         await new Promise(r => setTimeout(r, 500));
+        count++;
     }
 }
 
